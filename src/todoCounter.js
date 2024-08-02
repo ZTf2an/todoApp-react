7fadc9aa5af6f8aa30754ed173dafcfbc747964f
steffan <steffan.pardorios@gmail.com>
2024-08-02 10:32:26 -0500

    poniendo estilos en jsx desde un objeto externo

--- src/todoCounter.js
+++ src/todoCounter.js
@@ -1,7 +1,11 @@
+const estilos = {
+  // debe enviarse en un objeto y en camelcase, (en css sería background-color : 'red')
+  backgroundColor : 'red' ,
+}
 // las props "recibidas como parametro" deben ser como objeto, de la siguiente manera
 function TodoCounter ({ total , completed }) {
     return (
-      <h1>
+      <h1 style={estilos}>
         Has completado {completed} de {total} ToDo
       </h1>
     )
