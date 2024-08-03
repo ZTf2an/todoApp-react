51849cc981aca3efe283c9aea1674aa4252f8951
steffan <steffan.pardorios@gmail.com>
2024-08-02 21:48:36 -0500

    imprmiendo el texto del input .searcher en consola

--- src/todoSearch.js
+++ src/todoSearch.js
@@ -5,8 +5,6 @@ function TodoSearch () {
             className="searcher" 
             placeholder="cortar cebolla" 
             onChange={(event) => {
-                console.log('Escribiste en el TodoSearch');
-                console.log(event);
                 console.log(event.target.value)
             }}
         />
