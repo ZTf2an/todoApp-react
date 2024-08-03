47b5020fa6b1a600cf85a54b9afd1139036e89e2
steffan <steffan.pardorios@gmail.com>
2024-08-03 12:43:39 -0500

    borrando comentarios de todoSearch.js; enviando el codigo a app.js; poniendo a funcionar el todo counter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { TodoCounter } from './todoCounter';
 import { TodoSearch } from './todoSearch';
 import { TodoList } from './todoList';
@@ -8,19 +9,27 @@ import { CreateTodoButton } from './createTodoButton';
 const defaultTodos = [
   {text : 'cortar cebolla' , completed : true},
   {text : 'Tomar Curso de React Js' , completed : false},
-  {text : 'Llorar con la Llorona' , completed : false},
+  {text : 'Llorar con la Llorona' , completed : true},
   {text : 'Lalalala' , completed : false},
 ]
 
 function App() { 
+  const [todos , setTodos] = React.useState(defaultTodos);
+  const [searchValue , setSearchValue] = React.useState('');
+  console.log(`los usuarios buscan todos de ${searchValue}`);
+
+  const completedTodos = todos.filter(todo => todo.completed);
+  const totalCompletedTodos = completedTodos.length;
+  const totalTodos = todos.length
+
   // React para renderisarse solo debemos pasarle un elemento. por eso encapsulamos todo en el div .classname
   // si quisieramos que se renderisara todo sin estar dentro de un div, lo que debemos hacer es poner todo dentro de las etiquetas <React.Fragment></React.Fragment>, para esto deberíamos importar react
   // tambien sirve unicamente <></>
   return (
     <div className="App">
       {/* esta es la manera de isertar el componente al JSX */}
-      <TodoCounter completed={16} total={25} />
-      < TodoSearch />
+      <TodoCounter completed={totalCompletedTodos} total={totalTodos} />
+      < TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
 
       {/* <TodoList>
         <TodoItem />
--- src/todoSearch.js
+++ src/todoSearch.js
@@ -1,24 +1,12 @@
 import './styles/todoSearch.css'
-//importaremos react para usar "el estado"
-import React from 'react'
 
-function TodoSearch () {
-    //creamos la constante que nos traerá el estado
-    // const [state , setState ,] =React.useState();
-    //de la anterior manera se puede hacer, pero no es obligatorio el nombre
-
-    const [searchValue , setSearchValue] = React.useState('');
-
-    console.log(`los usuarios buscan todos de ${searchValue}`)
-
+function TodoSearch ({searchValue , setSearchValue}) {
     return (
         <input 
             className="searcher" 
             placeholder="cortar cebolla" 
-            //en la siguiente linea se programa el input para que el valor de searchValue del estado sea el que se está ingresando.
             value = { searchValue }
             onChange={(event) => {
-                //en esta linea se acualiza el estado ya que en useState() hay un string vacío, se actualizará el estado con lo que ingrese al input
                 setSearchValue(event.target.value)
             }}
         />
