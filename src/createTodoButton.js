a6a5d43ae6536f0673f05bdd6bc97ce5a7bb75e1
steffan <steffan.pardorios@gmail.com>
2024-08-02 21:32:14 -0500

    añadiendo evento onclick de ejemplo al button .createtodobutton

--- src/createTodoButton.js
+++ src/createTodoButton.js
@@ -1,7 +1,12 @@
 import './styles/createTodoButton.css'
 function CreateTodoButton () {
     return (
-      <button className='createTodoButton'>
+        
+      <button 
+        className='createTodoButton'
+        //los eventos se escriben en camelcase 
+        onClick={() => console.log('le diste click')}
+        >
         +
       </button>
     )
