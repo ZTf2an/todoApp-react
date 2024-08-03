39f8fc90ea3d08367c08c4515761e8eb55347971
steffan <steffan.pardorios@gmail.com>
2024-08-02 21:45:46 -0500

    añadiendo los eventos al input .searcher que está en todosearch.js

--- src/todoSearch.js
+++ src/todoSearch.js
@@ -1,7 +1,15 @@
 import './styles/todoSearch.css'
 function TodoSearch () {
     return (
-        <input className="searcher" placeholder="cortar cebolla"/>
+        <input 
+            className="searcher" 
+            placeholder="cortar cebolla" 
+            onChange={(event) => {
+                console.log('Escribiste en el TodoSearch');
+                console.log(event);
+                console.log(event.target.value)
+            }}
+        />
     )
 }
 
