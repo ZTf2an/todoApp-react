ede0ba4594463b846721ae8cfa02273af897eab3
steffan <steffan.pardorios@gmail.com>
2024-08-02 21:38:38 -0500

    añadiendo los eventos con el parametro event al boton .createtodobutton

--- src/createTodoButton.js
+++ src/createTodoButton.js
@@ -5,7 +5,14 @@ function CreateTodoButton () {
       <button 
         className='createTodoButton'
         //los eventos se escriben en camelcase 
-        onClick={() => console.log('le diste click')}
+        onClick={(event) => {
+            console.log('le diste click')
+            // nos devuelve el texto
+            console.log(event)
+            //nos devuelve el evento y todas las propiedades del evento
+            console.log(event.target)
+            //nos devuelve el elemento html desde donde se ejecutó, en este caso <button class="CreateTodoButton"></button>
+        }}
         >
         +
       </button>
