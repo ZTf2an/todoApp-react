97f8065f80eb7e27e32ce3d3108aeff197b97592
steffan <steffan.pardorios@gmail.com>
2024-08-02 22:14:18 -0500

    explicando como usar estado en src/todosearch.js

--- src/todoSearch.js
+++ src/todoSearch.js
@@ -1,11 +1,25 @@
 import './styles/todoSearch.css'
+//importaremos react para usar "el estado"
+import React from 'react'
+
 function TodoSearch () {
+    //creamos la constante que nos traerá el estado
+    // const [state , setState ,] =React.useState();
+    //de la anterior manera se puede hacer, pero no es obligatorio el nombre
+
+    const [searchValue , setSearchValue] = React.useState('');
+
+    console.log(`los usuarios buscan todos de ${searchValue}`)
+
     return (
         <input 
             className="searcher" 
             placeholder="cortar cebolla" 
+            //en la siguiente linea se programa el input para que el valor de searchValue del estado sea el que se está ingresando.
+            value = { searchValue }
             onChange={(event) => {
-                console.log(event.target.value)
+                //en esta linea se acualiza el estado ya que en useState() hay un string vacío, se actualizará el estado con lo que ingrese al input
+                setSearchValue(event.target.value)
             }}
         />
     )
