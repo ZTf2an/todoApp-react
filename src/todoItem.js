5c022a3995c4b71d426dc756fd17d1e5f7332b66
steffan <steffan.pardorios@gmail.com>
2024-08-09 19:36:12 -0500

    explicando como manipular el local Storage, la aplicacion ahora guarda la informacion en el local Storage

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,24 +1,14 @@
 import './styles/todoitem.css'
-//la siguiente es la manera de importar iconos de la librería react-icons
-// se debe importar react-icons con la npm
 import { FaCircleXmark } from "react-icons/fa6";
 import { FaCheck } from "react-icons/fa6";
 function TodoItem ({text , completed , onComplete , onDelete}) {
     return (
       <li className="TodoItem">
-        {/* <span 
-        className={`Icon Icon-check ${completed && "Icon-check--active"}`}
-        onClick={onComplete}
-        >V</span> */}
         <FaCheck
         className={`Icon Icon-check ${completed && "Icon-check--active"}`} 
         onClick={onComplete}
         />
         <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
-        {/* <span 
-        className='Icon Icon-delete'
-        onClick={onDelete}
-        >X</span> */}
         <FaCircleXmark 
         className='Icon Icon-delete'
         onClick={onDelete}
