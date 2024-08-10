f834bfc74762ade59819e5420dde64e89d619c2d
steffan <steffan.pardorios@gmail.com>
2024-08-09 20:50:31 -0500

    corrigiendo el counter, cuando no hay tareas cambia el mensaje

--- src/App.js
+++ src/App.js
@@ -13,47 +13,26 @@ import { CreateTodoButton } from './createTodoButton';
 //   {text : 'Lalalala' , completed : false},
 // ]
 
-//se comentan las lineas de la 9 - 14 ya que esto se le va a pasar directamente en la consola de la aplicacion de la siguiente manera:
-  //se copian las linas 9- 14 que es el array 
-  // en consola se pega y de esa manera queda declarada la variable
-  // let stringed = JSON.stringify(defaultTodos)
-  // la anterior linea se pega en la consola declarando una variable que almacena los datos del array y los convierte en string
-  // localStorage.setItem(stringed)
-  // la anterior linea se escribe en consola y de esa manera queda almacenado en el local storage
-  //al recargar la pagina aparecen los items
+// let stringied = JSON.stringify(defaultTodos); 
+// localStorage.setItem('TODOS_V1', stringied);
 
-//Acontinuacion unas ayudas para solo descomentar, copiar , pegar en la consola
-  // let stringied = JSON.stringify(defaultTodos); 
-  // localStorage.setItem('TODOS_V1', stringied);
-
-  // De la siguiente manera se resetea a la fuerza el TODOS_V1  
-    // localStorage.removeItem('TODOS_V1')
+// localStorage.removeItem('TODOS_V1')
 
 function App() { 
-//con la constante localStorageTodos almacenamos todos el valor en string del array que se almacena en TODOS_V1 en el local Storage de nuestra pagina
   const localStorageTodos = localStorage.getItem('TODOS_V1'); 
-  // la siguiente linea se comenta para ser utilizada en la linea 51
-  // let parsedTodos =  JSON.parse(localStorageTodos);
-
-  //esta linea genera la variable en la que los TODOS están en blanco al iniciar la aplicación con el local storage en blanco.
+  
   let parsedTodos = []
 
   if (!localStorageTodos) {
-    //en este bloque va toda la logica que se ejecuta cuando el localStorage está vacio en el TODOS_V1 del local Storage
-    // la siguiente linea pasa al local Storage el string vacio de los objetos 
     localStorage.setItem('TODOS_V1' , JSON.stringify([]))
-
-    //si hay algo en el estado, lo borramos en la siguiente linea
     parsedTodos = []
   } else {
-    //en este bloque se ejecuta toda la logica cuando el TODOS_V1 tiene informacion guardada.
-    //Se envia actualiza la informacion extrayendo el string que está en local Storage y se convierte en json nuevamente usando el .parse 
     parsedTodos = JSON.parse(localStorageTodos)
   }
 
   const [todos , setTodos] = React.useState(parsedTodos);
   const [searchValue , setSearchValue] = React.useState('');
-  console.log(`los usuarios buscan todos de ${searchValue}`);
+  // console.log(`los usuarios buscan todos de ${searchValue}`);
 
   const completedTodos = todos.filter(todo => todo.completed);
   const totalCompletedTodos = completedTodos.length;
@@ -64,7 +43,6 @@ function App() {
       .includes(searchValue.toLowerCase())
   )
 
-  //la siguiente funcion se genera para que podamos guardar la informacion en el localStorage y actualice el estado. se usa esta funcion (en vez de saveTodos) en la linea 80 y 89
   const saveTodos = (newTodos) => {
     localStorage.setItem('TODOS_V1' , JSON.stringify(newTodos));
 
--- src/todoCounter.js
+++ src/todoCounter.js
@@ -2,9 +2,12 @@ import './styles/TodoCounter.css'
 // las props "recibidas como parametro" deben ser como objeto, de la siguiente manera
 function TodoCounter ({ total , completed}) {
     return (
-      <h1>
-        Has completado {completed} de {total} ToDo
-        <div className={`${!(total === completed) && "inactive"}`}>
+      <h1 
+      // className={`${!total && "inactive"}`}
+      >
+        {(!total) ? "No hay tareas por realizar" : `Has completado ${completed} de ${total} ToDo` }
+        
+        <div className={`${!(total === completed) && "inactive"} ${!total && "inactive"}` }>
           Felicitaciones, has completado todas las tareas
         </div>
       </h1>
