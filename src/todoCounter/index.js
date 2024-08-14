34446baa2f98a6041a8c50d84a38ad988bbdb8ed
steffan <steffan.pardorios@gmail.com>
2024-08-13 20:46:48 -0500

    se corrigen algunos temas del todo counter

--- src/todoCounter/index.js
+++ src/todoCounter/index.js
@@ -1,14 +1,15 @@
 import './TodoCounter.css'
 // las props "recibidas como parametro" deben ser como objeto, de la siguiente manera
 function TodoCounter ({ total , completed}) {
+    let completado = total === completed
     return (
       <h1 
       // className={`${!total && "inactive"}`}
       >
         {(!total) ? "No hay tareas por realizar" : `Has completado ${completed} de ${total} ToDo` }
         
-        <div className={`${!(total === completed) && "inactive"} ${!total && "inactive"}` }>
-          Felicitaciones, has completado todas las tareas
+        <div className={`${!total && "inactive"}` }>
+          {completado && "Felicitaciones, has completado todas las tareas"}
         </div>
       </h1>
     )
